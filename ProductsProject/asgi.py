fdd1fb0e0c58c3b6f8419d5d7bad30409a111dad
bahaa ebraheem <Bahaa.ebraheem812@outlook.com>
2025-04-06 10:57:52 +0300

    commit

--- ProductsProject/asgi.py
+++ ProductsProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for firstPythonProject project.
+ASGI config for ProductsProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firstPythonProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ProductsProject.settings')
 
 application = get_asgi_application()
--- ProductsProject/urls.py
+++ ProductsProject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for firstPythonProject project.
+URL configuration for ProductsProject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
